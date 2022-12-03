4084a69f5fdcb5b2e407b868498a891eb6b84098
Emanuel Rocha <emanuelrocha831@gmail.com>
2022-12-02 21:42:09 -0300

    acervo, pefil, login, cadastro - ok ( antes de começar tarefas)

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Aluno\Documents\teste\eductechSenai-1\venv
+set VIRTUAL_ENV=C:\Users\emanu\OneDrive\Documentos\GitHub\eductechSenai\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
